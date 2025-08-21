5c680396c73e862ac0da4ffee1ce73f7efd18821
ThunderrZHub <dale101718@gmail.com>
2025-08-21 22:04:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 import requests
-from flask import Flask, request
-
+from flask import Flask, request, Response
 
 app = Flask(__name__)
-
+TARGET = "http://213.142.135.46:9999"  
 
 @app.before_request
-def before_request():
-    response = requests.request(
-        url="http://213.142.135.46:9999" + request.path,
+def proxy():
+    
+    url = f"{TARGET}{request.full_path}"
+    if url.endswith("?"):  
+        url = url[:-1]
+
+    
+    headers = dict(request.headers)
+    headers.pop("Host", None)
+    headers.pop("Content-Length", None)
+
+    
+    resp = requests.request(
         method=request.method,
+        url=url,
+        headers=headers,
         data=request.get_data(),
-        headers=request.headers
+        cookies=request.cookies,
+        allow_redirects=False,
+        stream=True,
     )
-    return response.text, response.status_code, {"Content-Type": response.headers.get("Content-Type")}
+
+    
+    excluded_headers = ["content-encoding", "transfer-encoding", "content-length", "connection"]
+    response_headers = [(name, value) for name, value in resp.raw.headers.items()
+                        if name.lower() not in excluded_headers]
+
+    return Response(resp.content, resp.status_code, response_headers)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000)
